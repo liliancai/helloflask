4011b92ea6753b95912c67a5eb72c23472ddf20b
liliancai <yushunchn@gmail.com>
2019-03-08 16:35:37 +0100

    database

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+
+import os 
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 '''
 from flask import redirect
 from flask import request
 '''
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'am a secret string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -40,6 +49,28 @@ class NameForm(FlaskForm):
     """
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('submit')
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+	# backref attribute can be used for users access role instead using role_id
+	# Role User one-to-many 12s, uselist =False -> 121
+	users = db.relationship('User', backref='role')
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+	
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 '''
 @app.route('/')
