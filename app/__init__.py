f485263ffe076b7e0e8fd5f5ea3f7629202891df
liliancai <yushunchn@gmail.com>
2019-03-14 15:40:50 +0100

    auth/login done, but need to update db

--- app/__init__.py
+++ app/__init__.py
@@ -4,26 +4,29 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_login import LoginManager
-db = SQLAlchemy()
-from .main import main as main_blueprint
-# from .auth import auth as auth_blueprint
 
+db = SQLAlchemy()
 bootstrap = Bootstrap()
 mail = Mail()
-print("Why you guess can't import me???")
+# print("Why you guess can't import me???")
+# cause import .main above db, and .main has # BUG:
+
 login_manager = LoginManager()
 
 def create_app(config_name):
 	app=Flask(__name__)
 	app.config.from_object(config[config_name])
 
+	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
-	# app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+	from .auth import auth as auth_blueprint
+	app.register_blueprint(auth_blueprint, url_prefix='/auth')
 	config[config_name].init_app(app)
 
 	bootstrap.init_app(app)
 	mail.init_app(app)
 	db.init_app(app)
-	#login_manager.init_app(app)
+	login_manager.init_app(app)
 
 	return app
