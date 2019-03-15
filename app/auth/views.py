ff0043a850e588effdf802049cbf1adcaa3fe810
liliancai <yushunchn@gmail.com>
2019-03-15 12:46:51 +0100

    auth/login done

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,25 @@
-from flask import render_template
+from flask import render_template, flash, url_for, redirect, request
 from . import auth
+from flask_login import login_user
 from .forms import LoginForm
+from ..models import User
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remeber_me.data)
+            next = request.args.get("next")
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        # else
+        flash('Invalid username or password :(')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+def logout():
+    pass
