7041115232268e5cca72c908ef49d96ed7de6254
liliancai <yushunchn@gmail.com>
2019-03-23 16:49:21 +0100

    qq

--- config.py
+++ config.py
@@ -12,13 +12,15 @@ class Config:
     # print(MAIL_SERVER)
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
     # print(MAIL_PORT)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+    #    ['true', 'on', '1']
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'true').lower() in \
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     # print(MAIL_USERNAME)
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <ccccccgggggg@163.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <2941758436@qq.com>'
     FLASKY_ADMIN = os.environ.get('FlASKY_ADMIN')
     # print(FLASKY_ADMIN)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
